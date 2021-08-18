3c7fd9386d5c44d35d2d9adb09e658c0678610ec
BADR KACIMI <31140256+BADRKAC@users.noreply.github.com>
2021-08-18 21:38:17 +0100

    updates names

--- src/main/java/eai/bam/commons/Constantes.java
+++ src/main/java/eai/bam/commons/Constantes.java
@@ -2,28 +2,28 @@ package eai.bam.commons;
 
 public class Constantes {
 
-	public static final String SERVER = "https://rccb.bankalmaghrib.ma/rccb-api/api/sendCCBData";
+	public static final String SERVER = "https://apiXXXX";
 	
-	public static final String CTR = "https://rccb.bankalmaghrib.ma/rccb-api/api/receiveCCBCTR";
+	public static final String CTR = "https://apiXXXX";
 	
 	public static final String PUBLIC_KEY = "./publique.bin"; //./src/main/resources/publique.bin
 	
 	public static final String JSON_FILE = "./JsonCCB.json";
 
-	public static final String file_CTR_old = "./ctrCCB-1"; 
+	public static final String file_CTR_old = "./ctr-1"; 
 	
-	public static final String file_CTR = "./ctrCCB";
+	public static final String file_CTR = "./ctr";
 	
 	/*	Table clients	*/
-	public static final String CLIENT_TABLE = "CLIENTSCCB4";
+	public static final String CLIENT_TABLE = "CLIENT";
 	
 	/*	Table comptes	*/
-	public static final String COMPTE_TABLE = "COMPTESCCB4";
+	public static final String COMPTE_TABLE = "COMPTE";
 	
 	/*	Table relation client compte	*/
-	public static final String CLIENT_COMPTE_TABLE = "CLIENTSCCB_COMPTESCCB4";
+	public static final String CLIENT_COMPTE_TABLE = "CLIENT_COMPTES";
 	
 	/*	Table headers	*/
-	public static final String HEADER_TABLE = "HEADERSCCB4";
+	public static final String HEADER_TABLE = "HEADER";
 	
 }
